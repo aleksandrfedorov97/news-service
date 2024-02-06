8830df3325e4b4d296ef508e8543da8ee6a29d01
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-02-06 09:08:53 +0300

    fix: AuthorizationUtils

--- src/main/java/com/example/newsservice/utils/AuthorizationUtils.java
+++ src/main/java/com/example/newsservice/utils/AuthorizationUtils.java
@@ -3,16 +3,19 @@ package com.example.newsservice.utils;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.experimental.UtilityClass;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @UtilityClass
 public class AuthorizationUtils {
 
     private static final String HTTP_USER_HEADER = "X-User-ID";
 
-    @Autowired
-    private HttpServletRequest request;
-
     public Long getCurrentUserId() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+
         String headerValue = request.getHeader(HTTP_USER_HEADER);
 
         if (headerValue == null || headerValue.isEmpty()) {
