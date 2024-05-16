8caf3b3a07c1e008a8e7af6558bd37cfbe2a5235
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2024-05-16 08:28:45 +0300

    feat: security (UserDetailsImpl.java, UserDetailsServiceImpl.java)

---
+++ src/main/java/com/example/newsservice/security/UserDetailsImpl.java
@@ -0,0 +1,55 @@
+package com.example.newsservice.security;
+
+import com.example.newsservice.model.User;
+import com.example.newsservice.model.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private final User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRoles().stream()
+                .map(Role::toAuthority).toList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
